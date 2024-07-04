e42109164dea933c69b32ddbab5c5c081ba383eb
Jay <kred110396@gmail.com>
2024-07-04 23:55:59 +0800

    feat: 🎸 update DraggableItemPanel

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -64,7 +64,7 @@ function DraggableItemPanel() {
       w="300px"
       gap="1rem"
     >
-      <Text alignSelf="flex-start" ml="0.75rem">
+      <Text alignSelf="flex-start" ml="0.75rem" mb="1rem">
         區塊數量 0 / 8
       </Text>
       <SimpleGrid columns={2} spacing={4}>
@@ -75,17 +75,21 @@ function DraggableItemPanel() {
           borderRadius="1.5rem"
           p="1rem 2rem"
         >
-          <Box
+          <Flex
             bgColor="white"
             borderColor="gray.100"
             borderWidth="1px"
             borderStyle="solid"
             borderRadius="1.5rem"
             boxShadow="rgba(50, 50, 0, 0.05) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px"
-            p="2rem 1rem"
+            justifyContent="center"
+            alignItems="center"
+            w="100px"
+            h="100px"
+            p="1rem"
           >
             文字按鈕
-          </Box>
+          </Flex>
         </Tooltip>
         <Tooltip
           hasArrow
@@ -94,17 +98,21 @@ function DraggableItemPanel() {
           borderRadius="1.5rem"
           p="1rem 2rem"
         >
-          <Box
+          <Flex
             bgColor="white"
             borderColor="gray.100"
             borderWidth="1px"
             borderStyle="solid"
             borderRadius="1.5rem"
             boxShadow="rgba(50, 50, 0, 0.05) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px"
-            p="2rem 1rem"
+            justifyContent="center"
+            alignItems="center"
+            w="100px"
+            h="100px"
+            p="1rem"
           >
             橫幅看板
-          </Box>
+          </Flex>
         </Tooltip>
       </SimpleGrid>
     </VStack>
